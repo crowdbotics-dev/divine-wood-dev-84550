c5546b5c9964530515b970d09bea20ce98eaaee8
Crowdbotics <engineering@crowdbotics.com>
2023-05-19 03:45:24 +0000

    Initial commit.

---
+++ backend/divine_wood_dev_84550/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for divine_wood_dev_84550 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'divine_wood_dev_84550.settings')
+
+application = get_wsgi_application()
